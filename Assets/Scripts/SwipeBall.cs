6f7ee07ba7c9835d975e92974edeab7a1a47faa9
HoangDao2105 <daohoang2105@gmail.com>
2023-06-15 20:17:14 +0700

    [Feature] Adding logic game

--- Assets/Scripts/SwipeBall.cs
+++ Assets/Scripts/SwipeBall.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class SwipeBall : MonoBehaviour
 {
+    [SerializeField] private int collideCount;
     private Vector2 startPos; // Vector2 start touch screen
     private Vector2 endPos; // Vector2 stop touch screen
     private Rigidbody2D _rgbd2d;
     private Camera cam;
     private bool isSwiped;
 
+    
+    
 
     public int powerForce;
-
+    public static event Action<int> OnBallHitWall; 
     private void Start()
     {
         isSwiped = false;
         cam = Camera.main;
         _rgbd2d = GetComponent<Rigidbody2D>();
+        OnBallHitWall?.Invoke(collideCount);
     }
 
     private void Update()
@@ -35,6 +39,35 @@ public class SwipeBall : MonoBehaviour
             isSwiped = true;
             // Add force to ball to make it move
             _rgbd2d.AddForce(swipe*powerForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //CollideCount minus one
+        collideCount--;
+        OnBallHitWall?.Invoke(collideCount);
+        if (collideCount < 0)
+        {
+            //Game Lose
+            Debug.Log("Game Lose");
+            Time.timeScale = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (collideCount==0)
+        {
+            //Game Won
+            Debug.Log("Game Won");
+            Time.timeScale = 0;
+        }
+        else
+        {
+            //Game Lose
+            Debug.Log("Game Lose");
+            Time.timeScale = 0;
         }
     }
 }
---
+++ Assets/Scripts/UIGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIGame : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI countTxt;
+    
+    private void OnEnable()
+    {
+        SwipeBall.OnBallHitWall += OnBallHitWall;
+    }
+
+    private void OnDisable()
+    {
+        SwipeBall.OnBallHitWall -= OnBallHitWall;
+    }
+
+    void OnBallHitWall(int count)
+    {
+        countTxt.text = count.ToString();
+    }
+}
