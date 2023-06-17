2b8c01e0bace298991fe0902030211cf0c762511
HoangDao2105 <daohoang2105@gmail.com>
2023-06-17 21:46:47 +0700

    [Feature] Adding addressable and load game scene

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Loading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CR_LoadGame(5));
+    }
+
+    IEnumerator CR_LoadGame(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        SceneManager.LoadScene("Main");
+    }
+}
